642ed775015aa2c0a78eb6ea86dd5e88d62221f6
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-25 07:53:00 +0000

    Add files via upload

---
+++ nested_list.py
@@ -0,0 +1,28 @@
+
+family1 = ["soumeya","rida","mehdi","sara"]
+family2 = ["khaled","zola","malak","chacha"]
+family3 = ["sliman","nesrine","maria","maram"]
+family4 = ["zino","selma","yusra","ines"]
+
+house1  =[family1,family2,family3,family4]
+
+print(house1)
+print("===============")
+print(house1[0])
+print("===============")
+print(family1[0])
+print("===============")
+print(family2[1])
+print("===============")
+print(family3[2])
+print("===============")
+print(house1[0][0])
+print("===============")
+print(house1[1][1])
+print("===============")
+print(house1[2][2])
+print("===============")
+print(house1[3][3])
+print("===============")
+print(family1[1][1])
+
